1e706bc0e86bc54fb68e966ae5dfea436baddb19
Vyas Raina <sayvaniar@gmail.com>
2022-07-27 23:53:48 +0100

    better ex

--- src/models/model_selector.py
+++ src/models/model_selector.py
@@ -1,7 +1,7 @@
 from .models import SequenceClassifier
 import torch
 
-def select_model(model_name='resnet', model_path=None):
+def select_model(model_name='bert-base-uncased', model_path=None):
     model =  SequenceClassifier(model_name=model_name)
     if model_path:
         model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
